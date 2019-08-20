737834a9dc8680fc673191d16851539170011811
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-20 02:10:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,7 @@ def countdown(number)
   number = 10
   while number > 0
   puts "#{number} SECOND(S)!"
-  "HAPPY NEW YEAR!"
   number -= 1
   end
+puts "HAPPY NEW YEAR!"  
 end
-puts "HAPPY NEW YEAR!"
